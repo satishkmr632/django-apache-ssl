b858600bc864f576df6039a90be1dbf1db3482fe
satishkmr632 <satishkmr632@gmail.com>
2019-06-06 15:02:59 +0530

    added * in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')d@%ig_rlaly477*_z&hh!qxkpc!*o_kykll4$+2&*-9vyq(6t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
